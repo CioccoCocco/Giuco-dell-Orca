b63c5f93df2f411d7cf74eabee6beb7a82f27e52
manuDesi03 <117749954+manuDesi03@users.noreply.github.com>
2023-01-23 21:10:59 +0100

    Update Giocatore.java

--- Giocatore.java
+++ Giocatore.java
@@ -1,92 +1,219 @@
 public class Giocatore
 {
-	private String nome;
-	private int soldi; 
-	private int pos;
-	private char carattere;
-	
-	public Giocatore(String name, char carattere)
-	{
-		this.nome = name;
-		this.carattere = carattere;
-		this.soldi = 0;
-		this.pos = 0;
-	}
-	
-	public int lanciaDado(Dado d)
-	{
-		return d.random(); // non capisco cos'ho sbagliato
-	}
-	
-	public void muovi(int nCaselle, int grandezzaCampo)
-	{
-		boolean esci = false;
-		
-		while(!esci) {
-			
-			if(pos == grandezzaCampo-1) {
-				pos -= 1;
-			}
-			else {
-				pos += 1;
-			}
-			
-			if(nCaselle <= 1)
-				esci = true;
-			
-			nCaselle -= 1;
-		}
-		
-		if(pos < 0)
-			pos = 0;
-	}
-	
-	public void chiediDomanda(Domandiere d)
-	{
-		String s = d.getDomanda(); // Andre riusciresti a mettere che returna la domanda al posto del void
-	}
-	
-	public void sfida(Giocatore g2, Dado d)
-	{
-		int p1 = 0;
-		int p2 = 0;
-		
-		for(int i = 0; i < 3; i++) {
-			p1 += d.random();
-			p2 += d.random();
-		}
-		
-		if(p1 > p2) {
-			setSoldi(g2.getSoldi(), true);
-			g2.setSoldi(g2.getSoldi(), false);			
-		}
-		else {
-			g2.setSoldi(getSoldi(), true);
-			setSoldi(getSoldi(), false);
-		}
-	}
-	
-	public int getSoldi()
-	{
-		return soldi;
-	}
-	
-	public void setSoldi(int s, boolean wl)
-	{ //se wl è true aumenta i soldi altrimenti li toglie 
-		if(wl) {
-			soldi += s;
-		}
-		else
-			soldi -= s;
-	}
-	
-	public boolean controlloVincita(int nCaselle)
-	{
-		return pos == nCaselle ?  true :  false;
-	}
-	
-	public void tornaAInizio()
-	{
-		pos = 0;
-	}
+    private String nome;
+    private int punti; 
+    private Casella casellaCorrente;
+    private char pedina;
+    private int[] domandeSvolte;
+    private int numdomandeSvolte;
+
+    public void Giocatore(String name, char pedina, Tabellone tab)
+    {
+        this.nome = name;
+        this.pedina = pedina;
+        this.punti = 0;
+        this.casellaCorrente = tab.casellaIn(0);
+        this.domandeSvolte = new int[90];
+    }
+
+    public String getNome( )
+    {
+        return nome;
+    }
+
+    public int getpunti( )
+    {
+        return punti;
+    }
+
+    public Casella getCasellaCorrente( )
+    {
+        return casellaCorrente;
+    }
+
+    public char getPedina( )
+    {
+        return pedina;
+    }
+
+    public int[] getDomadeSvolte( )
+    {
+        return domandeSvolte;
+    }
+
+    public void setNome( String nome )
+    {
+        this.nome = nome;
+    }
+
+    public void setpunti( int punti )
+    {
+        this.punti = punti;
+    }
+
+    public void setCasella( Casella casellaCorrente )
+    {
+        this.casellaCorrente = casellaCorrente;
+    }
+
+    public void setPedina( char pedina )
+    {
+        this.pedina = pedina;
+    }
+
+    public int lanciaDado(Dado d)
+    {
+        return d.lanciaDado();
+    }
+
+    public void muovi(int spostamento)
+    {
+        boolean indietro = false;
+
+        for(int i = 0; i < spostamento; i++)
+        {
+            if(!indietro)
+            {
+                if(casellaCorrente.getSuccessivo() == null)
+                {
+                    indietro = true;
+                }
+                else
+                {
+                    casellaCorrente = casellaCorrente.getSuccessivo();
+                }
+            }
+            if(indietro)
+            {
+                casellaCorrente = casellaCorrente.getPrecedente();
+            }
+        }
+    }
+
+    public void muoviImprevisto( int spostamento )
+    {
+        if(spostamento >= 1)
+        {
+            muovi(spostamento);
+        }
+        else
+        {
+            spostamento *= -1;
+            for(int i = 0; i < spostamento; i++)
+            {
+                if(casellaCorrente.getPrecedente() == null)
+                {
+                    return;
+                }
+                else
+                {
+                    casellaCorrente = casellaCorrente.getPrecedente();
+                }
+            }
+        }
+    }
+    
+    public boolean DomandeSvolte ( int k )
+    {
+        for(int i = 0; ds[i] != null && i < ds.length; i++)
+        {
+            if(ds[i] == k)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Domanda chiediDomanda(Domandiere domandiere)
+    {
+        do
+        {
+            Domanda domanda = domandiere.getDomanda();
+        }
+        while(DomandeSvolte (domanda.getIdentificativo()));
+        return domandiere.getDomanda();
+    }
+
+    public boolean rispondi(int risposta, Domanda domanda)
+    {
+        domandeSvolte[numDomandeSvolte] = domanda.getIdentificativo();
+        numDomandeSvolte++;
+        return domanda.corretta( risposta );
+    }
+
+    public int altroGiocatore(Giocatore[] giocatori, int indice)
+    {
+        for(int i = 0; i < giocatori.length; i++)
+        {
+            if(i != indice)
+            {
+                if(casellaCorrente.equals(giocatori[i].getCasellaCorrente()))
+                {
+                    return i;
+                }
+            }
+        }
+    }
+
+    public boolean sfida( Giocatore g2, Dado d )
+    {
+        int p1 = 0, p2 = 0;
+        int t1, t2;
+
+        for(int i = 0; i < 3; i++)
+        {
+            t1 = d.lanciaDado();
+            t2 = d.lanciaDado();
+
+            if( t1 > t2 )
+            {
+                p1++;
+            }
+            else
+            {
+                p2++;
+            }
+        }
+
+        if(p1 > p2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public void napoli( Giocatore g2 )
+    {
+        punti += g2.getPunti();
+        g2.setPunti(0);
+    }
+
+    public boolean controlloVincita()
+    {
+        if(casellaCorrente.getSuccessiva() == null)
+        {
+            if(punti >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                tornaAInizio();
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void tornaAInizio()
+    {
+        CasellaCorrente = 0;
+    }
 }
